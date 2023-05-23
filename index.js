bd9b9f1790689166454b2a53726f0ea68dfe7471
subramanian1503 <roopan1503@gmail.com>
2023-05-23 12:58:14 +0530

    Initial commit: Created required directories and initialised project with npm and git

---
+++ index.js
@@ -0,0 +1,16 @@
+// Requiring the express to launch the server
+const express = require('express');
+
+// Declaring a port on which the server need to listen
+const port = 8080;
+
+// Initialise the express applicaition
+const app = express();
+
+// Express applicaiton listening the port 
+app.listen(port, function (error) {
+    if (error) {
+        console.log(`Error occured while trying to listen the port ${port} and the error logs are ${error}`);
+    }
+    console.log(`Server listening the port ${port} successfully`);
+});
